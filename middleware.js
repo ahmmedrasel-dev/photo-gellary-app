4ac5f93618bdb92f7c0927d0b0869a77731aa173
ahmmedrasel-dev <rahmmed.info@gmail.com>
2025-02-21 23:25:32 +0600

    create middleware for internationalization project

---
+++ middleware.js
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+let locales = ["en", "bn"];
+let defaultLocale = "en";
+
+function getLocale(request) {
+  // Get the language from the request headers
+  const acceptLanguage = request.headers.get("accept-language") ?? undefined;
+  // Make headers object with the accept-language key
+  let headers = { "accept-language": acceptLanguage };
+  // Get the language array from browser using Negotiator
+  let languages = new Negotiator({ headers }).languages();
+  // Return the matched language
+  return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request) {
+  // Read path name from the request object.
+  const pathName = request.nextUrl.pathname;
+  const pathNameIsMissingLocale = locales.every(
+    (local) => !pathName.startsWith(`/${local}`) && pathName !== `/${local}`
+  );
+
+  if (pathNameIsMissingLocale) {
+    const locale = getLocale(request);
+    // Redirect to the same path with the locale prefix.
+    return NextResponse.redirect(
+      new URL(`/${locale}/${pathName}`, request.nextUrl)
+    );
+  }
+}
+
+export const config = {
+  matcher: [
+    // Skip all the Internal Paths (_next, api, assets)
+    "/((?!_next).*)",
+  ],
+};
